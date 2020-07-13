4cd4d57e48380daf73163800c36a4f8afb1edeb7
Mustafa Kemal GILOR <mgilor@nettsi.com>
2020-07-13 15:02:25 +0300

    Initial

---
+++ app/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * ______________________________________________________
+ * Example application.
+ *
+ * @file         main.cpp
+ * @author       Mustafa Kemal GILOR <mgilor@nettsi.com>
+ * @date         13.07.2020
+ *
+ * @copyright    2020 NETTSI Informatics Technology Inc.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * ______________________________________________________
+ */
+
+#include <iostream>
+
+/**
+ * @brief Main entry point of the application
+ *
+ * @return int exit code
+ */
+auto main() -> int{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
