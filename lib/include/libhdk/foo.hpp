c6a19de45ff71a859f3068b01a9c81aded335217
Mustafa Kemal GILOR <mgilor@nettsi.com>
2020-07-13 18:39:19 +0300

    Added an example library project. Started implementing various unit test examples with different parameters. Documented the "behind-the-scenes" operations of the created target types.

---
+++ lib/include/libhdk/foo.hpp
@@ -0,0 +1,29 @@
+/**
+ * ______________________________________________________
+ * 
+ * 
+ * @file         foo.hpp
+ * @author       Mustafa Kemal GILOR <mgilor@nettsi.com>
+ * @date         13.07.2020
+ * 
+ * @copyright   2020 NETTSI Informatics Technology Inc.
+ * 
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * ______________________________________________________
+ */
+
+#pragma once
+
+namespace hdk{
+    
+    /**
+     * An example class.
+     */
+    class foo{
+    public:
+        foo();
+        ~foo();
+        void bar();
+    };
+}
---
+++ lib/src/foo.cpp
@@ -0,0 +1,27 @@
+/**
+ * ______________________________________________________
+ * Example application.
+ *
+ * @file         main.cpp
+ * @author       Mustafa Kemal GILOR <mgilor@nettsi.com>
+ * @date         13.07.2020
+ *
+ * @copyright    2020 NETTSI Informatics Technology Inc.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * ______________________________________________________
+ */
+
+#include <libhdk/foo.hpp>
+
+namespace hdk{
+
+    foo::foo() = default;
+    
+    foo::~foo() = default;
+
+    void foo::bar(){
+        // body.
+    }
+}
